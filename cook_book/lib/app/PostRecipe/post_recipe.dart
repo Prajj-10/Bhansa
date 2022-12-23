a17fe049e506f497184a482e4e566688640b74f3
unknown <iamshahirohit@gmail.com>
2022-12-23 12:22:23 +0545

    Updated with Image Upload and Dynamic TextField

--- cook_book/lib/app/PostRecipe/post_recipe.dart
+++ cook_book/lib/app/PostRecipe/post_recipe.dart
@@ -1,8 +1,16 @@
 import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:cook_book/main.dart';
+
+import 'storage_services.dart';
+
+
 import 'package:flutter/material.dart';
 import 'package:snippet_coder_utils/FormHelper.dart';
+import 'package:file_picker/file_picker.dart';
+
+import 'cookingSteps_model.dart';
 
 class PostRecipe extends StatefulWidget {
   const PostRecipe({Key? key}) : super(key: key);
@@ -14,12 +22,31 @@ class PostRecipe extends StatefulWidget {
 class _PostRecipeState extends State<PostRecipe> {
 
   //int currentStep = 0;
+  CollectionReference recipeDetails = FirebaseFirestore.instance.collection('recipe_details');
+
+  String? ingredients, recipe_title;
+  int? num_of_servings;
+
+  GlobalKey<FormState> globalKey = new GlobalKey<FormState>();
+  CookingStepsModel steps_model = CookingStepsModel();
+
+
+
+  @override
+  void initState(){
+    steps_model.cooking_steps = new List<String>.empty(growable: true);
+    steps_model.cooking_steps!.add("");
+    //steps_model.cooking_steps!.add("");
+  }
+
 
   @override
   Widget build(BuildContext context) {
 
     var width = MediaQuery.of(context).size.width;
     var height = MediaQuery.of(context).size.height;
+
+    final Storage storage = Storage();
 
     return Scaffold(
       //backgroundColor: Colors.black,
@@ -202,53 +229,85 @@ class _PostRecipeState extends State<PostRecipe> {
                       height: 10,
                     ),
 
-                    Container(
-                      height: 50,
-                      child: const TextField(
-
-                        style: TextStyle(fontSize: 20, color: Colors.white),
-
-                        decoration: InputDecoration(
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              width: 2,
-                              color: Colors.white,
-                            ),
-                          ),
-
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white),
-                          ),
-                        ),
-                      ),
+                    ListView.separated(
+                        shrinkWrap: true,
+                        physics: const ScrollPhysics(),
+                        itemBuilder: (context, index){
+                          return Column(
+                            children: [_directions(index)],
+                          );
+                        },
+                        separatorBuilder: (context, index)=> const Divider(),
+                        itemCount: steps_model.cooking_steps!.length
                     ),
 
+                    const SizedBox(
+                      height: 20,
+                    ),
 
 
 
                     Container(
                       child: Row(
                         children: [
-                          Text(
+                          const Text(
                             "Upload Image: ",
                             style: TextStyle(fontSize: 20, color: Colors.white),
                           ),
-
-                          SizedBox(
+                          const SizedBox(
                             width: 15,
                           ),
 
-                          IconButton(
+                          /*IconButton(
                               onPressed: (){
                                 //print('Hello');
-                                _getFromGallery();
+                                _getFromCamera();
                               },
                               color: Colors.white,
-                              icon: Icon(Icons.upload, size: 35,)
-                          )
+                              icon: Icon(Icons.camera_alt, size: 35,)
+                          ),*/
+
+                          IconButton(
+                              onPressed: ()async{
+                                final results = await FilePicker.platform.pickFiles(
+
+                                  allowMultiple: false,
+                                  type: FileType.custom,
+                                  allowedExtensions: ['png', 'jpg'],
+                                );
+
+                                if(results == null){
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                      const SnackBar(
+                                        content: Text("No Photo selected"),
+                                      )
+                                  );
+
+                                  return null;
+                                }
+
+                                final path = results.files.single.path!;
+                                final fileName = results.files.single.name;
+
+                                storage
+                                    .uploadImage(path, fileName)
+                                    .then((value)=>print('Done'));
+
+                              },
+                              color: Colors.white,
+                              icon: const Icon(Icons.file_upload, size: 35,)
+                          ),
+
                         ],
                       ),
                     ),
+
+
+
+
+
+
+
 
                     const SizedBox(
                       height: 50,
@@ -270,14 +329,99 @@ class _PostRecipeState extends State<PostRecipe> {
     );
   }
 
-  _getFromGallery() async {
-    PickedFile? pickedFile = await ImagePicker().getImage(
-      source: ImageSource.gallery,
-      maxWidth: 1800,
-      maxHeight: 1800,
+  Future<void> addRecipe() {
+    // Call the user's CollectionReference to add a new user
+    return recipeDetails
+        .add({
+      'Title': recipe_title,
+      'Number of Servings': num_of_servings,
+      'Ingredients': ingredients // 42
+    })
+        .then((value) => print("Posted"))
+        .catchError((error) => print("Failed to add Recipe: $error"));
+  }
+
+  Widget _directions(index){
+    //var index;
+    return Padding(
+      padding: EdgeInsets.all(0),
+
+      child: Row(
+        children: [
+          Flexible(
+            child: FormHelper.inputFieldWidget(
+
+              context,
+              "steps_$index",
+              "",
+                  (onValidate){
+                if(onValidate.isEmpty){
+                  return 'Step ${index +1} cannot be empty!!!';
+                }
+
+                return null;
+              },
+                  (onSavedVal){
+                steps_model.cooking_steps![index] = onSavedVal;
+              },
+              borderColor: Colors.white,
+              borderFocusColor: Colors.white,
+              borderRadius: 0,
+              fontSize: 16,
+
+            ),
+          ),
+
+          Visibility(
+            child: IconButton(
+              icon: Icon(
+                Icons.add_circle,
+                color: Colors.lightGreenAccent,
+                size: 30,
+              ),
+              onPressed: (){addSteps();},
+            ),
+            visible: index == steps_model.cooking_steps!.length -1,
+          ),
+
+
+
+          /*SizedBox(
+                  width: 10,
+                ),*/
+
+          Visibility(
+            child: IconButton(
+              icon: Icon(
+                Icons.remove_circle,
+                color: Colors.redAccent,
+                size: 30,
+              ),
+              onPressed: (){removeSteps(index);},
+            ),
+            visible: index > 0,
+          ),
+
+
+        ],
+      ),
+
+
     );
-    if (pickedFile != null) {
-      File imageFile = File(pickedFile.path);
-    }
+  }
+
+  void addSteps(){
+    setState(() {
+      steps_model.cooking_steps!.add("");
+    });
+  }
+
+  void removeSteps(index){
+    setState(() {
+      if(steps_model.cooking_steps!.length > 1){
+
+        steps_model.cooking_steps!.removeAt(index);
+      }
+    });
   }
 }
