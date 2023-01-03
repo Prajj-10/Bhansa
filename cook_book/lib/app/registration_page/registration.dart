9a98e79a96bb19bae39bf16813ee554ec57345a6
Prajj-10 <prajjwalveerbasnet10@gmail.com>
2023-01-03 17:43:05 +0545

    Functionality to check username created.

--- cook_book/lib/app/registration_page/registration.dart
+++ cook_book/lib/app/registration_page/registration.dart
@@ -325,40 +325,47 @@ class _RegistrationPageState extends State<RegistrationPage> with InputValidatio
   }
   void signUp(String email, String password) async {
     if (formKey.currentState!.validate()) {
-      try {
-        await auth
-            .createUserWithEmailAndPassword(email: email, password: password)
-            .then((value) => {postDetailsToFireStore()})
-            .catchError((e) {
-          Fluttertoast.showToast(msg: e!.message);
-        });
-      } on FirebaseAuthException catch (error) {
-        switch (error.code) {
-          case "invalid-email":
-            errorMessage = "Your email address appears to be malformed.";
-            break;
-          case "wrong-password":
-            errorMessage = "Your password is wrong.";
-            break;
-          case "user-not-found":
-            errorMessage = "User with this email doesn't exist.";
-            break;
-          case "user-disabled":
-            errorMessage = "User with this email has been disabled.";
-            break;
-          case "too-many-requests":
-            errorMessage = "Too many requests";
-            break;
-          case "operation-not-allowed":
-            errorMessage = "Signing in with Email and Password is not enabled.";
-            break;
-          default:
-            errorMessage = "An undefined Error happened.";
-        }
-        Fluttertoast.showToast(msg: errorMessage!);
-        print(error.code);
+      bool usernameTaken = await isUsernameTaken("@${userNameController.text}") as bool;
+      if(usernameTaken){
+        Fluttertoast.showToast(msg: "This username is taken.");
       }
-    }
+      else{
+        try {
+          await auth
+              .createUserWithEmailAndPassword(email: email, password: password)
+              .then((value) => {postDetailsToFireStore()})
+              .catchError((e) {
+            Fluttertoast.showToast(msg: e!.message);
+          });
+        } on FirebaseAuthException catch (error) {
+          switch (error.code) {
+            case "invalid-email":
+              errorMessage = "Your email address appears to be malformed.";
+              break;
+            case "wrong-password":
+              errorMessage = "Your password is wrong.";
+              break;
+            case "user-not-found":
+              errorMessage = "User with this email doesn't exist.";
+              break;
+            case "user-disabled":
+              errorMessage = "User with this email has been disabled.";
+              break;
+            case "too-many-requests":
+              errorMessage = "Too many requests";
+              break;
+            case "operation-not-allowed":
+              errorMessage = "Signing in with Email and Password is not enabled.";
+              break;
+            default:
+              errorMessage = "An undefined Error happened.";
+          }
+          Fluttertoast.showToast(msg: errorMessage!);
+          print(error.code);
+        }
+      }
+      }
+
   }
   postDetailsToFireStore() async{
     // call FireStore
@@ -387,7 +394,7 @@ class _RegistrationPageState extends State<RegistrationPage> with InputValidatio
             (route) => false);
   }
 
-  Future<bool> isUsernameTaken(String username) async {
+   isUsernameTaken(String username) async {
     // Query the Firestore database for a document with the desired username
     final QuerySnapshot snapshot = await firestore
         .collection('users')
@@ -396,6 +403,26 @@ class _RegistrationPageState extends State<RegistrationPage> with InputValidatio
 
     // If the snapshot is not empty, it means the username is already taken
     return snapshot.docs.isNotEmpty;
+  }
+
+  doesUsernameExist(String username) async {
+    // Perform the query
+    QuerySnapshot querySnapshot = await firestore
+        .collection('users')
+        .where('username', isEqualTo: username)
+        .get();
+
+    // If the query returns a non-empty list of documents, it means that the username exists
+    return querySnapshot.docs.isNotEmpty;
+  }
+
+  checkUsernameExists(String value) {
+    return doesUsernameExist(value).then((exists) {
+      if (exists) {
+        return 'This username is already taken';
+      }
+      return "";
+    });
   }
 
 
