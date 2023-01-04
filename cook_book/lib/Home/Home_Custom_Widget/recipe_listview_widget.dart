44a49b488d3aee56184abc207cc0ff84922bcfc7
Bikash <bikashchaudhary007@gmail.com>
2023-01-05 01:06:04 +0545

    Recipe List Redirected to Recipe Details page of Home

--- cook_book/lib/Home/Home_Custom_Widget/recipe_listview_widget.dart
+++ cook_book/lib/Home/Home_Custom_Widget/recipe_listview_widget.dart
@@ -4,6 +4,7 @@ import 'package:cook_book/Home/Home_Custom_Widget/small_text.dart';
 import 'package:cook_book/Home/Home_Custom_Widget/small_text2.dart';
 import 'package:flutter/material.dart';
 
+import '../../app/RecipeDetail_Page/recipeDetails.dart';
 import 'icon_and_text.dart';
 
 class RecipeListViewWidget extends StatefulWidget {
@@ -31,80 +32,86 @@ class _RecipeListViewWidgetState extends State<RecipeListViewWidget> {
             //shrinkWrap: true,
               itemCount: snapshot.data?.docs.length,
               itemBuilder: (context, index){
-                return Container(
-                  margin: EdgeInsets.only(left: 20, right: 20, bottom: 10),
-                  child: Row(
-                    children: [
-                      //Image Section
-                      Container(
-                        width: 100,
-                        height: 100,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            color: Colors.white,
-                            image: DecorationImage(
-                              fit : BoxFit.cover,
-                              image: NetworkImage('${snapshot.data?.docs[index]['Photo']}'),
-                            )
-                        ),
-                      ),
-
-                      //Text Section
-                      Expanded(
-                        child: Container(
-                            height: 100,
-                            //width:200,
-                            margin: EdgeInsets.only(left: 10),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.only(
-                                  topRight: Radius.circular(20),
-                                  bottomRight: Radius.circular(20)
-                              ),
+                final DocumentSnapshot recipe_Snapshot = snapshot.data!.docs[index];
+                return GestureDetector(
+                  onTap: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=> RecipeDetails(recipe_snapshot: recipe_Snapshot),),);
+                  },
+                  child: Container(
+                    margin: EdgeInsets.only(left: 20, right: 20, bottom: 10),
+                    child: Row(
+                      children: [
+                        //Image Section
+                        Container(
+                          width: 100,
+                          height: 100,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
                               color: Colors.white,
-                            ),
-
-                            child: Padding(
-                              padding: EdgeInsets.only(left: 10, right: 10),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  BigText(text: "${snapshot.data?.docs[index]['Title']}", color: Colors.black, size: 18,),
-                                  SizedBox(height: 10,),
-                                  SmallText2(text: "${snapshot.data?.docs[index]['Description']}"),
-                                  SizedBox(height: 10,),
-                                  Row(
-                                    children: [
-                                      IconAndTextWidget(
-                                          icon: Icons.circle_sharp,
-                                          text: "Normal",
-                                          iconColor: Colors.orangeAccent),
-
-                                      SizedBox(width: 2,),
-                                      IconAndTextWidget(
-                                          icon: Icons.cookie_outlined,
-                                          text: "${snapshot.data?.docs[index]['Number of Servings']}",
-                                          iconColor: Colors.lightBlue),
-
-                                      SizedBox(width: 2,),
-                                      IconAndTextWidget(
-                                          icon: Icons.access_time_rounded,
-                                          text: "${snapshot.data?.docs[index]['Total Duration']}",
-                                          iconColor: Colors.greenAccent)
-
-                                    ],
-                                  )
-                                ],
-                              ),
-                            )
+                              image: DecorationImage(
+                                fit : BoxFit.cover,
+                                image: NetworkImage('${snapshot.data?.docs[index]['Photo']}'),
+                              )
+                          ),
                         ),
 
+                        //Text Section
+                        Expanded(
+                          child: Container(
+                              height: 100,
+                              //width:200,
+                              margin: EdgeInsets.only(left: 10),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.only(
+                                    topRight: Radius.circular(20),
+                                    bottomRight: Radius.circular(20)
+                                ),
+                                color: Colors.white,
+                              ),
 
-                      )
+                              child: Padding(
+                                padding: EdgeInsets.only(left: 10, right: 10),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    BigText(text: "${snapshot.data?.docs[index]['Title']}", color: Colors.black, size: 14,),
+                                    SizedBox(height: 10,),
+                                    SmallText2(text: "${snapshot.data?.docs[index]['Description']}"),
+                                    SizedBox(height: 10,),
+                                    Row(
+                                      children: [
+                                        IconAndTextWidget(
+                                            icon: Icons.circle_sharp,
+                                            text: "Normal",
+                                            iconColor: Colors.orangeAccent),
+
+                                        SizedBox(width: 2,),
+                                        IconAndTextWidget(
+                                            icon: Icons.cookie_outlined,
+                                            text: "${snapshot.data?.docs[index]['Number of Servings']}",
+                                            iconColor: Colors.lightBlue),
+
+                                        SizedBox(width: 2,),
+                                        IconAndTextWidget(
+                                            icon: Icons.access_time_rounded,
+                                            text: "${snapshot.data?.docs[index]['Total Duration']}",
+                                            iconColor: Colors.greenAccent)
+
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              )
+                          ),
+
+
+                        )
 
 
 
-                    ],
+                      ],
+                    ),
                   ),
                 );
               }
